dfdfede2b901699a23961ae8d1a746c2ab2b5514
mishnan07 <mishnanc@gmail.com>
2024-10-23 10:27:05 +0530

    server model

--- Models/model.js
+++ Models/model.js
@@ -4,17 +4,6 @@ const { Schema } = mongoose;
 const dataSchema = new Schema(
   {
     intensity: Number,
-    // likelihood: Number,
-    // relevance: Number,
-    // end_year: String,
-    // sector: String,
-    // topic: String,
-    // region: String,
-    // country: String,
-    // city: String,
-    // pestle: String,
-    // source: String,
-    // swot: String,
   },
   { timestamps: true, collection: "datas" }
 );
