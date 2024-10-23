71cd5f200e947af3311996a7c31c9b746759ba25
mishnan07 <mishnanc@gmail.com>
2024-10-23 10:23:38 +0530

    model

--- Models/model.js
+++ Models/model.js
@@ -4,17 +4,17 @@ const { Schema } = mongoose;
 const dataSchema = new Schema(
   {
     intensity: Number,
-    likelihood: Number,
-    relevance: Number,
-    end_year: String,
-    sector: String,
-    topic: String,
-    region: String,
-    country: String,
-    city: String,
-    pestle: String,
-    source: String,
-    swot: String,
+    // likelihood: Number,
+    // relevance: Number,
+    // end_year: String,
+    // sector: String,
+    // topic: String,
+    // region: String,
+    // country: String,
+    // city: String,
+    // pestle: String,
+    // source: String,
+    // swot: String,
   },
   { timestamps: true, collection: "datas" }
 );
